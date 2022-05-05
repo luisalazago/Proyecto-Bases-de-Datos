f318c1554c5113a6f4dce6f25a5b23f0b24f8a37
nicolemolineros <48654860+nicolemolineros@users.noreply.github.com>
2022-05-04 20:28:42 -0500

    NEW: Upload Producto Values

---
+++ Primera Entrega/grupo3insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO Producto VALUES (001, 'Valorant', 'Shooter', 'Shooter multijugador 5v5 basado en personajes de Riot Games', 120000, 16 );
+INSERT INTO Producto VALUES (002, 'Warzone', 'Shooter','Permite el combate multijugador online entre 150 jugadores', 80000, 18);
+INSERT INTO Producto VALUES (003, 'Battlefront 1', 'Shooter','Es un videojuego donde se narran las películas de Star Wars', 15000, 13);
+INSERT INTO Producto VALUES (004, 'Battlefront 2','Shooter','Es un videojuego de disparos en tercera y primera persona.', 25000, 13);
+INSERT INTO Producto VALUES (005, 'Halo Infinite','Shooter','El juego es secuela de la historia de Halo 5 y Halo Wars 2',35000, 16);
+INSERT INTO Producto VALUES (006, 'Counter Strike', 'Shooter', 'Es un videojuego de disparos en primera persona multijugador', 40000, 18 );
+INSERT INTO Producto VALUES (007, '7 days to die', 'Terror','Es un videojuego de acción en primera y tercera persona.', 30000, 13);
+INSERT INTO Producto VALUES (008, 'Terraria','Aventura','Es un videojuego de mundo abierto en 2D.', 60000, 12);
+INSERT INTO Producto VALUES (009, 'Minecraft', 'Sandbox','Es un videojuego de construcción de tipo mundo abierto', 67000, 10);
+INSERT INTO Producto VALUES (010, 'League of Legends', 'Multijugador','Es un videojuego de arena de batalla en línea.', 12000, 13);
+
+
