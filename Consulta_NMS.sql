800a949ce46f7c5cf27a5739aa8877b127948977
nicolemolineros <48654860+nicolemolineros@users.noreply.github.com>
2022-05-18 10:15:32 -0500

    Update Consulta_NMS.sql

--- Consulta_NMS.sql
+++ Consulta_NMS.sql
@@ -1,13 +1,13 @@
 ALTER TABLE Usuario 
-MODIFY telefono VARCHAR(22);
+MODIFY telefono VARCHAR(22); -- Modifica el tamaño de la cadena de teléfono.
 
     
 INSERT INTO Usuario
-VALUES(1000000100, 'Juan S', 'juan.s@pipiolo.com', 'SpongeBob', 23, 'Colombia', 'Cliente','1', '1111111');
+VALUES(1000000100, 'Juan S', 'juan.s@pipiolo.com', 'SpongeBob', 23, 'Colombia', 'Cliente','1', '1111111'); --Usuario de prueba random 
 
 -----------------------------------------------
 
--- Mas compras listo
+-- Mas compras 
 
 INSERT INTO CONSUME VALUES (1000000000, 3,3100000010);
 INSERT INTO CONSUME VALUES (1000000000, 5,3100000011);
@@ -20,9 +20,7 @@ INSERT INTO CONSUME VALUES (1000000575, 6,3100000017);
 INSERT INTO CONSUME VALUES (1000500000, 8,3100000018); 
 INSERT INTO CONSUME VALUES (1020000489, 1,3100000019);
 
--- Mas transacciones listo
-
--- despues 3100000007
+-- Mas transacciones 
 
 INSERT INTO Transacciones
 VALUES(3100000010, 890088, 009748, '15-12-2020', 25000);
@@ -61,16 +59,14 @@ INSERT INTO Biblioteca VALUES(1020000489,003);
 INSERT INTO Biblioteca VALUES(1000000001,005);
 INSERT INTO Biblioteca VALUES(1005000201,001);
 --------------------------------------------------
---Consultas
-
 --CONSULTAS
 
 --1 Listar los datos de los juegos que han sido comprados por más de 5 usuarios (si no maneja la figura de compra, use la figura que permite el uso del juego)
 WITH Juegos AS(SELECT COUNT(*) AS Compras, idJuego
 FROM Consume
 GROUP BY idJuego
-HAVING COUNT(*) >2)
-SELECT idjuego, compras, producto.titulo, producto.genero, producto.descripcion, producto.precio, producto.restriccionedad
+HAVING COUNT(*) >2) -- Se hizo con 2 y no con 5 porque no hay mas de 2
+SELECT idjuego, compras, producto.titulo, producto.genero, producto.descripcion, producto.precio, producto.restriccionedad -- Son los datos del juego
 FROM Producto INNER JOIN Juegos
 USING(idJuego);
 
@@ -79,15 +75,15 @@ USING(idJuego);
 
 SELECT telefono
 FROM Usuario
-WHERE NOT REGEXP_LIKE (telefono, '^\(\d{2}\)(\s+)\d{3}(\s+)-\d{7}$');
+WHERE NOT REGEXP_LIKE (telefono, '^\(\d{2}\)(\s+)\d{3}(\s+)-\d{7}$'); -- Se pasa el formato correcto para comparar
 
 --11  Hacer un ranking de los clientes según la cantidad de juegos adquiridos en el año anterior. Liste los 5 clientes con mayor cantidad.
  
-WITH r AS (
-SELECT idusuario, COUNT(*) AS Cantidad,  RANK() OVER (ORDER BY COUNT(*) DESC) as ranking
+WITH r AS ( -- r sería el ranking
+SELECT idusuario, COUNT(*) AS Cantidad,  RANK() OVER (ORDER BY COUNT(*) DESC) as ranking --Se usa la función RANK para sacar el top 5 de clientes.
 FROM Consume JOIN Transacciones ON(consume.idtransaccion = transacciones.idtransaccion)
 WHERE EXTRACT(YEAR FROM fecha) = 2021
 GROUP BY idusuario)
 SELECT r.idusuario, Cantidad, ranking, nombre
 FROM r JOIN usuario ON (r.idusuario = usuario.idusuario)
-ORDER BY ranking ASC;
+ORDER BY ranking ASC; -- Se hace esto para que imprima en orden ascendente el ranking de los clientes
